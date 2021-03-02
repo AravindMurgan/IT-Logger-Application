9b0b7d0780d85c2765841c98f742382357cbb169
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-03-03 00:12:35 +0530

    addlogmodaldonewithmothersblessing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 const App = () => {
-  useEffect(()=>{
-
-    M.AutoInit();
-  })
+	useEffect(() => {
+		M.AutoInit();
+	});
 	return (
 		<>
 			<SearchBar />
 			<div className='container'>
 				<AddBtn />
+				<AddLogModal />
 				<Logs />
 			</div>
 		</>
